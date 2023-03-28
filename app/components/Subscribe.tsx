1efe0990adb5c2004bc6b2f936779ffa98c13c17
soundsystems <jaay.g@disroot.org>
2023-03-28 03:38:24 -0500

    Subscribe button works
    -added lottie loading spinner animation
    -form should now  submit correctly

--- app/components/Subscribe.tsx
+++ app/components/Subscribe.tsx
@@ -1,5 +1,6 @@
 import { InboxArrowDownIcon } from "@heroicons/react/24/outline";
 import { useFormFields, useMailChimpForm } from "use-mailchimp-form";
+import "@lottiefiles/lottie-player";
 
 export default function Subscribe() {
   const url = process.env.NEXT_PUBLIC_MAILCHIMP_URL as string;
@@ -15,7 +16,7 @@ export default function Subscribe() {
     EMAIL: "",
   });
   return (
-  <div className="my-6 mx-auto flex-col justify-center text-center"> 
+  <div className="my-6 mx-auto"> 
     <form onSubmit={event => {
         event.preventDefault();
         handleSubmit(fields);
@@ -28,31 +29,37 @@ export default function Subscribe() {
 <span 
     className="px-2 inline-flex text-xs sm:text-sm md:text-lg lg:text-xl font-semibold uppercase text-orange-500 transition duration-500 group-hover:text-orange-100" 
   > 
-            Subscribe
+            {loading ? (
+              <>
+                <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_jsuj2bs7.json"  background="transparent"  speed="1"  style={{width: "24px", height: "24px"}}  loop  autoplay></lottie-player> 
+                <span>submitting</span> 
+              </>
+            ) : "Subscribe" }
           </span> 
           <span className="px-2 inline-flex"> 
-            <input 
-              className="bg-transparent p-1 text-center text-sm text-orange-500 placeholder:text-orange-500 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-orange-500" 
-                      id="EMAIL"
-                      autoFocus
-                      required
-                      type="email"
-                      value={fields.EMAIL}
-                      onChange={handleFieldChange}
-                      placeholder="Join the club"
-                    /> 
+            {loading ? null : (
+              <input 
+                className="bg-transparent p-1 text-center text-sm text-orange-500 placeholder:text-orange-500 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-orange-500" 
+                        id="EMAIL"
+                        autoFocus
+                        required
+                        type="email"
+                        value={fields.EMAIL}
+                        onChange={handleFieldChange}
+                        placeholder="Join the club"
+                      /> 
+            )} 
                   </span> 
           <span className="px-2 ml-auto inline-flex"> 
             <InboxArrowDownIcon className="h-8 w-8 text-orange-500 transition duration-500 group-hover:text-orange-100" /> 
           </span> 
         </button> 
-      {loading && "submitting"}
-      {error && message}
-      {success && message}
+        <div className="text-center font-medium text-zinc-900 mt-2">
+      {error && <span>{message}</span>}
+      {success && <span>{message}</span>}
+    </div>
       </form> 
     
   </div>  
   );
-}
-
-
+            }
