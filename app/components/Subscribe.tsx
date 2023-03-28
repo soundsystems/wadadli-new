8831e3c60a15c043511a241cccd1f1a759fa1aca
soundsystems <jaay.g@disroot.org>
2023-03-28 02:50:36 -0500

    Testing use-mailchimp-form
    reworking subscribe component

--- app/components/Subscribe.tsx
+++ app/components/Subscribe.tsx
@@ -1,50 +1,26 @@
 import { InboxArrowDownIcon } from "@heroicons/react/24/outline";
-import { FormEventHandler, FormEvent, useState } from "react";
+import { useFormFields, useMailChimpForm } from "use-mailchimp-form";
 
 export default function Subscribe() {
-  const [state, setState] = useState<"idle" | "loading" | "success" | "error">("idle");
-  const [email, setEmail] = useState("");
-  const [errorMsg, setErrorMsg] = useState("");
-
-  const subscribe: FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-
-    setState("loading");
-    setErrorMsg("");
-
-    try {
-      const res = await fetch("/api/newsletter", {
-        method: "POST",
-        body: JSON.stringify({ email }),
-      });
-      const data = await res.json();
-
-      if (res.ok) {
-        setState("success");
-        setEmail("");
-      } else {
-        setErrorMsg(data.error || "Oops! Something went wrong.");
-        setState("error");
-      }
-    } catch (e) {
-      console.error("Error subscribing to newsletter", e);
-      setErrorMsg("Oops! Something went wrong.");
-      setState("error");
-    }
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.currentTarget.value);
-  };
-
+  const url = process.env.NEXT_PUBLIC_MAILCHIMP_URL as string;
+  
+  const {
+    loading,
+    error,
+    success,
+    message,
+    handleSubmit
+  } = useMailChimpForm(url);
+  const { fields, handleFieldChange } = useFormFields({
+    EMAIL: "",
+  });
   return (
-    <div className="my-6 mx-auto"> 
-    {state === "success" ? (
-      <p className="mt-4 text-center text-xl font-medium text-green-800">
-        Thanks for joining the club. You will now be notified of upcoming events.
-      </p> 
-    ) : (
-      <form onSubmit={subscribe}> 
+  <div className="my-6 mx-auto flex-col justify-center text-center"> 
+    <form onSubmit={event => {
+        event.preventDefault();
+        handleSubmit(fields);
+    }}
+        > 
 <button 
   type="submit" 
   className="group relative w-48 sm:w-56 md:w-96 mx-auto inline-flex items-center justify-center divide-x divide-orange-500 rounded-xl bg-zinc-50/60 py-1 px-40 sm:px-44 sm:py-2 leading-none shadow-lg shadow-zinc-900/75 transition duration-500 hover:divide-orange-200 hover:bg-zinc-900/90 hover:shadow-md overflow-hidden"  
@@ -61,8 +37,8 @@ export default function Subscribe() {
                       autoFocus
                       required
                       type="email"
-                      value={email}
-                      onChange={handleChange}
+                      value={fields.EMAIL}
+                      onChange={handleFieldChange}
                       placeholder="Join the club"
                     /> 
                   </span> 
@@ -70,11 +46,11 @@ export default function Subscribe() {
             <InboxArrowDownIcon className="h-8 w-8 text-orange-500 transition duration-500 group-hover:text-orange-100" /> 
           </span> 
         </button> 
-        {state === "error" && (
-          <p className="mt-3 text-center text-red-500">{errorMsg}</p> 
-        )} 
+      {loading && "submitting"}
+      {error && message}
+      {success && message}
       </form> 
-    )} 
+    
   </div>  
   );
 }
