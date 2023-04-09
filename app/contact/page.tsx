ac281ff3f84ddc801fd5143d95b9aa17f3b40f42
soundsystems <jaay.g@disroot.org>
2023-04-08 22:57:15 -0500

    Bug Fix: Fix location link for safari
    Added check for isIOSDevice

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,16 +7,26 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Map from '../components/Map';
 
+
 function isMobileDevice() {
   if (typeof navigator === 'undefined') {
     return false;
   }
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  
+}
+function isIOSDevice() {
+  if (typeof navigator === 'undefined') {
+    return false;
+  }
+  return /iPhone|iPad|iPod/i.test(navigator.userAgent);
 }
 
 const mapsLink = isMobileDevice()
-  ? 'geo:40.68452871196172,-73.94436153324439'
-  : 'https://www.google.com/maps?q=419+Putnam+Ave+Brooklyn,+NY+11216';
+  ? isIOSDevice()
+    ? 'maps://maps.apple.com/?daddr=40.68452871196172,-73.94436153324439'
+    : 'https://maps.google.com/maps?daddr=40.68452871196172,-73.94436153324439'
+  : 'https://www.google.com/maps/place/Wadadli+Jerk/@40.6848716,-73.9470323,17z/data=!3m1!4b1!4m6!3m5!1s0x89c25bf027b56619:0xd79555c3b11d9932!8m2!3d40.6848676!4d-73.9444574!16s%2Fg%2F11tfn_q95s';
 
 
 const Contact: NextPage = () => {
