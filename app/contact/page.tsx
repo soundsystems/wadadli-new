e4b026fbeb502c4504fa29bec7de0204ff7d9252
soundsystems <jaay.g@disroot.org>
2023-04-17 17:07:15 -0500

    MarkerIcon update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -24,10 +24,9 @@ function Map() {
   if (!isLoaded) return <div>Loading...</div>;
 
   const markerIcon = {
-    url: 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 24 24\' fill=\'%23FD6F00\' width=\'25px\' height=\'40px\'%3E%3Cpath d=\'M12 0C5.4 0 0 5.4 0 12C0 18.6 12 24 12 24C12 24 24 18.6 24 12C24 5.4 18.6 0 12 0ZM12 17.3C9.6 17.3 7.7 15.4 7.7 13C7.7 10.6 9.6 8.7 12 8.7C14.4 8.7 16.3 10.6 16.3 13C16.3 15.4 14.4 17.3 12 17.3Z\' /%3E%3C/svg%3E',
+    url: 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 384 512\' fill=\'%23FD6F00\' width=\'24px\' height=\'48px\'%3E%3Cpath d=\'M192 0C86 0 0 86 0 192c0 69.7 44.6 126.6 123.6 155.2L192 512l68.4-164.8C339.4 318.6 384 261.7 384 192 384 86 298 0 192 0zm0 277c-47.3 0-85.7-38.3-85.7-85.7 0-47.3 38.3-85.7 85.7-85.7 47.3 0 85.7 38.3 85.7 85.7 0 47.3-38.3 85.7-85.7 85.7z\' /%3E%3Ccircle cx=\'192\' cy=\'192\' r=\'53\' fill=\'%232B2726\' /%3E%3C/svg%3E',
     scaledSize: new google.maps.Size(25, 40),
-  };
-  
+  };  
 
   return (
     <GoogleMap
